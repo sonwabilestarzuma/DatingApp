b1c6d22ec88c154d71ac772b67a1733ebbdeb075
Sonwabo Zuma <sonwabilestarzuma@outlook.com>
2019-06-19 15:20:54 +0200

    Added Validation and Tesded using Post man

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,21 +19,23 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
                 // validate request 
+                if (!ModelState.IsValid)
 
-                username = username.ToLower();
+                return BadRequest(ModelState);
+                userForRegisterDto.UserName= userForRegisterDto.UserName.ToLower();
 
-                if (await _repo.UserExists(username))
+                if (await _repo.UserExists(userForRegisterDto.UserName))
                 return BadRequest("Username already exists");
 
                 var userToCreate = new User
                 {
-                    UserName = username
+                    UserName = userForRegisterDto.UserName
                 };
 
-                var createdUser = await _repo.Register(userToCreate, password);
+                var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
                 return StatusCode(201);
         }
     }
